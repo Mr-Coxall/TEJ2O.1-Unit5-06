2b03ce751695b13da5c35a2e40b94b53227527ab
Patrick Coxall <mr.coxall@mths.ca>
2025-10-15 11:04:20 -0400

    init

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
+ * Created by: Mr Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program finds the distance using a sonar.
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// find distance
+input.onButtonPressed(Button.A, function() {
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.clearScreen()
+    basic.showNumber(distanceToObject)
+    basic.showIcon(IconNames.Happy)
+})
